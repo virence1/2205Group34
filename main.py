9fe593639af38e36ea178971924ae07b649d06a7
Rxelxius <62737559+Rxelxius@users.noreply.github.com>
2023-01-31 17:29:05 +0800

    Fuse sections under Common

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ def index():
   return render_template('index.html')
 
 @app.route("/about")
-
 def about():
   return render_template('about.html')
-  
+
 if __name__ == "__main__":
-  app.run(host="127.0.0.1", port=5000, debug=False)
+  app.run(host="127.0.0.1", port=80, debug=False)
