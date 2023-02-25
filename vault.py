a00091232a0a97c82cdfb0c3283cece63904d653
Pavulraj Shamita <pavulrajshamita@gmail.com>
2023-02-25 11:54:06 +0800

    Updated vault.py
    invoke functions based on random order generated ensure that path is random all the time. also shows we never hard code anything

--- vault.py
+++ vault.py
@@ -1,4 +1,5 @@
 import requests
+import random
 
 def connect():
     return
@@ -10,8 +11,39 @@ def generate():
 def store():
     return
 
+def generatePath():
+    nodes=[1,2,3]
+    pathToDest = random.sample(nodes,len(nodes))
+    sendData(tuple(pathToDest))
+    for n in pathToDest:
+        print(n)
+    
+def sendData(order):
+    mapping = {
+        (1,2,3) : sendToNode1,
+        (1,3,2) : sendToNode1,
+        (2,1,3) : sendToNode2,
+        (2,3,1) : sendToNode2,
+        (3,1,2) : sendToNode3,
+        (3,2,1) : sendToNode3
+    }
+    function = mapping.get(order)
+    if function is not None:
+        function()
+    else:
+        print(f"Invalid order: {order}")
+    
 
-def sendToServer():
+def sendToNode2():
+    print("Node 2 Function invoked.")
+    pass
+
+def sendToNode3():
+    print("Node 3 Function invoked.")
+    pass
+
+def sendToNode1():
+    print("Node 1 Function invoked.")
     url = "http://20.81.121.55/endpoint"
     data ={'message':'Hello, world!'}
     response = requests.post(url, json=data)
@@ -24,4 +56,5 @@ def sendToServer():
     
     return
 
-sendToServer()
+
+generatePath()
