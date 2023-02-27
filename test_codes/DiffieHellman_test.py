533ebc3167cefe95189a127ba28e7a5525220a3e
virence1 <virencelouis@gmail.com>
2023-02-27 14:30:56 +0800

    Added 1st test code

---
+++ test_codes/DiffieHellman_test.py
@@ -0,0 +1,44 @@
+import sys
+import os
+sys.path.append(os.path.join('..', 'diffiehellman'))
+import unittest
+from diffiehellman.diffiehellman import DiffieHellman
+
+
+class TestDiffieHellman(unittest.TestCase):
+    def setUp(self):
+        self.alice = DiffieHellman()
+        self.bob = DiffieHellman()
+
+    def test_equality_of_keys(self):
+        self.alice.generate_public_key()
+        self.bob.generate_public_key()
+
+        alices_shared_key = self.alice.generate_shared_secret(self.bob.public_key)
+        bobs_shared_key = self.bob.generate_shared_secret(self.alice.public_key)
+
+        self.assertEqual(alices_shared_key, bobs_shared_key,
+                         "There is a mismatch between two shared secrets. Both shared secrets should be the same. This is bad.")
+
+    def test_decorators_private_key(self):
+        self.alice.generate_public_key()
+        self.assertIn("_DiffieHellman__private_key", self.alice.__dict__)
+
+    def test_generate_private_key(self):
+        self.alice.generate_private_key()
+        self.assertIn("_DiffieHellman__private_key", self.alice.__dict__)
+
+    def test_generate_public_key(self):
+        self.alice.generate_public_key()
+        self.assertIn("public_key", self.alice.__dict__)
+
+    def test_verify_public_key(self):
+        self.alice.generate_public_key()
+        self.bob.generate_public_key()
+        self.assertTrue(self.alice.verify_public_key(self.bob.public_key))
+        self.assertFalse(self.alice.verify_public_key(2))
+        self.assertFalse(self.alice.verify_public_key(self.alice.prime - 1))
+
+
+if __name__ == '__main__':
+    unittest.main()
