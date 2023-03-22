9af06152c46674dcbc502f44daa80b24d0c1d346
virence1 <virencelouis@gmail.com>
2023-03-22 20:02:24 +0800

    Create Tx_DH.py

---
+++ DH/Tx/Tx_DH.py
@@ -0,0 +1,12 @@
+import base_DH
+import math
+import requests
+from random import randint
+
+p = base_DH.gen_prime(2000, 6000) #modulus
+g = base_DH.gen_prime(500, 1000) #base
+
+Tx_privK = base_DH.gen_prime(1000, 3000)
+Tx_pubK = base_DH.generate_public_key(Tx_privK, p, g)
+
+shared_key_Tx = base_DH.decode_public_key(Rx_pubK, Tx_privK, p)
