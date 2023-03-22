af01949593263c0f54a20e5e75508489736b9481
virence1 <virencelouis@gmail.com>
2023-03-22 19:56:45 +0800

    Pseudocode added from P and G value

--- DH/Rx_DH.py
+++ DH/Rx_DH.py
@@ -3,11 +3,14 @@ import math
 import requests
 from random import randint
 
-p = base_DH.gen_prime(2000, 6000) #modulus
-g = base_DH.gen_prime(500, 1000) #base
+#p = base_DH.gen_prime(2000, 6000) #modulus
+#g = base_DH.gen_prime(500, 1000) #base
+p = get.modulusfromsomewhere
+g = get.basefromsomewhere
 
 Rx_privK = base_DH.gen_prime(1000, 3000)
-Rx_pubK = base_DH.generate_public_key(Rx_privK, p, g)
-    
 
-    
+Rx_pubK = base_DH.generate_public_key(Rx_privK, p, g)
+key2 = base_DH.decode_public_key(Tx_pubK, Rx_privK, p)
+
+shared_key_Rx = base_DH.decode_public_key(Tx_pubK, Rx_privK, p)
