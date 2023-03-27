29aa5c6b24d1341dea156a74983999e26ee59bd9
JesS <90232280+waili990@users.noreply.github.com>
2023-03-28 03:39:12 +0800

    Delete templates directory

--- templates/backups/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-   echo "config.php reached!";
-   session_start();
-   define('SERVER', 'localhost');
-   define('USERNAME', 'root');
-   define('PASSWORD', '');
-   define('DATABASE', 'user_accounts');
-?>
